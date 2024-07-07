6efcfeab00a696e2fdf5e01db61a8e1dc889c593
Dafne Cuvas <U21225921@utp.edu.pe>
2024-07-06 21:19:27 -0500

    Modificacion de clases

--- src/main/java/com/alura/literatura/model/Autor.java
+++ src/main/java/com/alura/literatura/model/Autor.java
@@ -1,5 +1,55 @@
 package com.alura.literatura.model;
 
-public class Autor {
+import jakarta.persistence.*;
 
+import java.util.List;
+
+@Entity
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+    @OneToMany(mappedBy = "autor")
+    private List<Libro> libros;
+
+    @OneToOne(mappedBy = "autor")
+    private DatosAutor datosAutor;
+
+
+    //Getter and Setter
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(List<Libro> libros) {
+        this.libros = libros;
+    }
+
+    public DatosAutor getDatosAutor() {
+        return datosAutor;
+    }
+
+    public void setDatosAutor(DatosAutor datosAutor) {
+        this.datosAutor = datosAutor;
+    }
 }
